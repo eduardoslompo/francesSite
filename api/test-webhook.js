34f9217045acd274150a1251f81aea3ee936ced4
Eduardo <eduardo5slompo@gmail.com>
2025-03-19 10:14:03 -0300

    update webhook

--- api/test-webhook.js
+++ api/test-webhook.js
@@ -1,12 +1,12 @@
 // Este é um script para testar o webhook localmente
 // Execute com: node api/test-webhook.js
 
-// Simular as variáveis de ambiente
-process.env.FIREBASE_SERVICE_ACCOUNT_KEY = JSON.stringify({
+// Simular as variáveis de ambiente - Modificado para funcionar no PowerShell
+const serviceAccountKey = {
   "type": "service_account",
   "project_id": "aprenderfrances-site",
-  "private_key_id": "eb1a96f37838d6757bf5cf07844cef21cd1c0f74",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCjznxnWlen3/ct\nGUbXuRpyaDoxaN3DMWhdpXH+CHNkJ7t4Kv/rR8GNYf72HRbFqc94arQxfQHt8W0o\nobQ9tt9pzcvRct3P57auip+wbl/Dd3PNwU84TKvh3FZepR9lcERDrbyRnUn6IG2a\nI/F1eAgPYyNVh7h2b5EWS2QzjMTp6tUdnXXnRQs/rDkIXyNtMpPzoz/km5blj4gU\nJaxndt7tiGG0e8ju5AAZI1wbpU3PDovhvz2pxNRTVMz06xBSyPjq5MlhPzO05HBL\no9phsOLasgxnkWwTu0urVccPf2y50aKU7vR701T2P7/+rinpW+U84FtD4vKYIwEx\nmGDrnE7/AgMBAAECggEAD5U0k3ZuMUumjXVCnoH+6JROuOWTdx6roYD8K6DtLUF0\nXZWlm+HLIWKJqSsSQAKL+JYaBz0vok2dx0g+x8IuVbfcpFOrkzwlTkxkrZmKZaNm\n8lNp8F6m2UknNEC8EF/SNqf/8NBIOdG863Vw4m5lQeV59oh/PVEWucq5QQgqsV/c\n+oGl2XnUQzLoyLSUObD/TPT6OzFcoAQ3r1181w/a5mgtjcMdK3lDmr4+LUZHAPyx\nbruTwE01LvK+Jk5COBXrg2cT9BLqFT9dQ/n4ffF0iq2EzkJhIpPxiFB+byic/Hak\nAFs/n9DgIvtgp/vWH9fC0QlU8gzMtYumxdxupLUVMQKBgQDhi6OmFMQyvrD8jWjq\n82JY/c0DoO5ETJr4GTjeJxHRdwfOQ4m3wtXzaW4nPBnydOdxqGglUo8R3ASrp9jY\nJkctoReRrm6jtnFYzBGS2hb3ATAdc8/LPIsyAeADKx6qu6YiTB0DmmTYHzvSZ5oN\n/tvH5k44h6Pw0/ViFd4zoYQiEQKBgQC57LzhyCC7D7SMi9LEPTzxHMsJ0BrNDIKB\nQZCXx+6MuSm2FE2eArVVDrsvL3kdLHafOe9qJAxzuT2l8wa922d4Kne7lTbsVdoT\nB3SyFrQzlEZadg74+0csJlP2Cvlg8KfGtKNgqzWedHIh41no46sZjFsSj9Pi72wK\n/21e3alQDwKBgHDqYdGBz/+/rAekHASUOIZo+bx5Uw/qciMHLYs6cBc3yjafZVEU\nP9yOydDmq+7QTBaAwzL8NBgy2Du/2jXMZBIZ9hy/ATEA45obAClO+fRLKfV0x3Du\nLJmqiqXLbpz3OOMuy8/D+fyhFmft6hhboVhgmoR+k7q7wruKSyX6FJahAoGAGnRZ\n59Je/d7dNMZMZgnEmxYaubm3podqJlQa683R3ooWKkc3SA1gmcqvzaX2mLUT59IJ\ng9O/RdQMtoke43LNhSadxL4Vg4kvr1d3PkPGvjQR0ptLxBeU5+5IMhEbrLRxWu+m\n84FI7tPozXOY/m8ukEMNTlXSs06X0/TGLiJiDfcCgYEAwP9Ey48yrtfJDvRFM9K9\npggGP6FiDMRg0m6TTp3ABRtnymEY6fKcpRgrS3qTvcMbHbFDbHg/rZ+Sa8U1jgtX\n5QI8LAlAJ1W1UFnUbLFrohzMA7DZGEM3ix9koggp526KThYyVcjmHR+gQy41UtpN\nmy7PIoz+yr2wrL0bsF6H5dA=\n-----END PRIVATE KEY-----\n",
+  "private_key_id": "7c82ada3ec7bf612b5c3ec294b275b186fbc139b",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDTduQ7+TL4knY5\nQdnelRG6MXvdOPw5mI/zdImvKHfUZypbgH9TDUMniSABBHd+vSk6XKwMNIRgX60p\nIRvWN8cgDb9g4hcblvbLNtvCH5J3J2ZmfKUSCq1zJ1vIMCQArQFbWNkDmc390T6Z\nm+L4JDaav+XS1yNjhh33b924qkw65vYmRoh34rZPdpv6myBYxvahGila8roP9vVk\nFwE3pnpnTQFNIgsIHmTWwFsccfPKH4UyaJO/d7NdO/UyEKnwUNKqkns4HpbnYdJf\nFeVRNkN79XeoIGA/IplXB8v7WQ4KBwmCvGtV7A5bqa5QQ8HwPukykhsiWe+MW9DU\nh2FV2381AgMBAAECggEACeYjch5iKFa3S5/7pCyMtR2fOqXHQqYTwpQgVlFNbWVx\nJ5yCoHL+mMfAaB2QagNq4af4QPNYaUS/iLt37gPM3ivRPt92lFkNy9eDBdYeE+S+\nm2pBp35yBUcygNLfM7FXXs7Fmsz91GA8a6AynwLehbrgZ98SHD+UAKZ9ZAzZO5Os\ndxsvSDy7vtjwvSB5tVbg1j8ZsxxQkF8ZYMY1JzybDg5iBSZT7NXkt/zWOfuMAp+n\nPaftob1uEkEYTkKz07/K4x3yFEs5Bn6RgyAIHkuHDaoTvyBp2o8OOE8K65g/O/ho\nGhXBULeP4/2rts14wZnds3BydinulzpXrp5JpOJbRQKBgQDs79U/1IFU6L9WHisy\nIHob9VTE7fMJ+zAJnS8wDu3VsGaIk1nsrEbSgu2Km8os81nJkBveRGr2pIMmoNIR\nwkMmc9xxDJ2oO2nbjqUtMbVffINSlolXiblBb1QBYG5noryzpEI4Ww23S7527YQL\nGKqGEKdJIjnIAHEZPzzEVddZPwKBgQDkeme9FTwz2PD1ahvlid6nrPjrYDZCm+Sx\n1vFo9G495F58qKfdkGGW+SMndULH8lP7HB2AWpgBDWLbsbaUv6/5zBwydpHV8wOi\n9+IJKHFu+S+WF4UO0iumTcCO1UlM2U5j8INtmjZcZyMMEOuRpbrGSTnemvd4636p\n0WlWE3V2iwKBgEi6sdXfEDgVHa58ojvH1lPAsd2lQOY+cZdx5Ws2GfFvLXoc6QyK\nMU8rG2lhuuSjaltB3hwTGlZNV2jDEtiarewewnr9KWPboPnhRnih+o+t882lIdF6\nGCCq4LlvdVRgktX9xMnxwTsTiAqPi+mwD6AJeqArXlszL4RVcfy+cNUXAoGBAKdR\npMrp1C67/qoazNcqQi5VE+17hgD9nVtHZ39PEbStmyx7boNpiq4/QSlSTFeU9T99\n9XTb41lR7kWpPWZtgnfSrAugnWgrEUy1b4j5Xyi/v8OQ8dZKv8CITC3NsGJg+XFi\n3zzSpGDC/v2FHp6I2eEAGysUHYsmGppOS2/HzEvFAoGBANr6c1+yObyGT0SP/STp\nWioOZEHA1f5BRPe1NuIBWf+Cr/NRakEA3+n+0OyRRS2/lFQg2I6kNvacao+sb/k/\nd1fpN+cST2+yNBK/jNHZTsZ1xRfJQ5dmcwCDhfBxwVqvNBRrzixaMVEDLOWbG+9t\nA8JHCrs0Ly/Gx8lD5Mpu7P+1\n-----END PRIVATE KEY-----\n",
   "client_email": "firebase-adminsdk-fbsvc@aprenderfrances-site.iam.gserviceaccount.com",
   "client_id": "104151318863688404864",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -14,8 +14,10 @@ process.env.FIREBASE_SERVICE_ACCOUNT_KEY = JSON.stringify({
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40aprenderfrances-site.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
-});
+};
 
+// Atribuir diretamente o objeto em vez de usar JSON.stringify
+process.env.FIREBASE_SERVICE_ACCOUNT_KEY = JSON.stringify(serviceAccountKey);
 process.env.FIREBASE_DATABASE_URL = 'https://aprenderfrances-site.firebaseio.com';
 
 // Importar a função do webhook
@@ -27,7 +29,7 @@ const mockReq = {
   body: {
     data: {
       buyer: {
-        email: 'teste@exemplo.com',
+        email: 'teste' + Date.now() + '@exemplo.com',
         name: 'Usuário de Teste'
       },
       product: {
@@ -63,4 +65,4 @@ async function runTest() {
   }
 }
 
-runTest(); 
+runTest();
